1b3fe86ddaf2e638a0381f870563f6d12f43dd02
AdrianJS <ajimsan2009@g.educaand.es>
2023-02-02 08:42:43 +0100

    Intento de arreglo de error en archivos.

--- Programación/Ejercicios y Proyectos/Java/cajaFuerte - Orientado a Objetos/app/src/main/java/com/politecnicomalaga/cajaFuerte/CajaFuerte.java
+++ Programación/Ejercicios y Proyectos/Java/cajaFuerte - Orientado a Objetos/app/src/main/java/com/politecnicomalaga/cajaFuerte/CajaFuerte.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.politecnicomalaga.cajaFuerte;
 
-/**
- *
- * @author adriair-school
- */
+package com.politecnicomalaga.cajafuerte;
+
 public class CajaFuerte {
     
     private String password;
