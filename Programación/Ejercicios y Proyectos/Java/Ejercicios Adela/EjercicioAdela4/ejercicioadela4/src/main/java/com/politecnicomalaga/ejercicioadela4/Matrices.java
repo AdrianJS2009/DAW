ef62038afbaccece4b36815cc026a39d79056884
AdrianJS <ajimsan2009@g.educaand.es>
2023-02-13 12:53:57 +0100

    Update

--- Programación/Ejercicios y Proyectos/Java/Ejercicios Adela/EjercicioAdela4/ejercicioadela4/src/main/java/com/politecnicomalaga/ejercicioadela4/Matrices.java
+++ Programación/Ejercicios y Proyectos/Java/Ejercicios Adela/EjercicioAdela4/ejercicioadela4/src/main/java/com/politecnicomalaga/ejercicioadela4/Matrices.java
@@ -2,29 +2,31 @@ package com.politecnicomalaga.ejercicioadela4;
 
 import java.util.Random;
 
-public class Matrices {
-    private int[][] matrizAleatoria;
-    private char[][] matrizPrimo;
+import javax.sound.sampled.SourceDataLine;
 
-    public Matrices() {
-        matrizAleatoria = new int[5][5];
-        matrizPrimo = new char[5][5];
+public class Matrices { 
+    private int[][] matrizAleatoria; // Matriz de números aleatorios
+    private char[][] matrizPrimo; // Matriz de caracteres que indica si el número de la matrizAleatoria es primo o no
+
+    public Matrices() { 
+        matrizAleatoria = new int[5][5]; 
+        matrizPrimo = new char[5][5]; 
         Random random = new Random();
 
         // Rellenar matrizAleatoria con valores aleatorios
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < 5; i++) { 
+            for (int j = 0; j < 5; j++) { // Rellena la matrizAleatoria con números aleatorios entre 1 y 100
                 matrizAleatoria[i][j] = random.nextInt(100) + 1;
                 matrizPrimo[i][j] = 'N';
             }
         }
     }
-
-    public boolean isPrimo(int numero) {
+    // Comprueba si un número es primo
+    public boolean isPrimo(int numero) { 
         if (numero <= 1) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(numero); i++) {
+        for (int i = 2; i <= Math.sqrt(numero); i++) { // Comprueba si el número es divisible por algún número entre 2 y su raíz cuadrada
             if (numero % i == 0) {
                 return false;
             }
@@ -32,7 +34,7 @@ public class Matrices {
         return true;
     }
 
-    public void rellenarMatrizPrimo() {
+    public void rellenarMatrizPrimo() { // Rellena la matrizPrimo con S si el número de la matrizAleatoria es primo
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 if (isPrimo(matrizAleatoria[i][j])) {
@@ -42,7 +44,7 @@ public class Matrices {
         }
     }
 
-    public void mostrarMatrices() {
+    public void mostrarMatrices() { // Muestra las matrices
         System.out.println("Matriz aleatoria:");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
@@ -50,7 +52,7 @@ public class Matrices {
             }
             System.out.println();
         }
-        System.out.println("Matriz primo:");
+        System.out.println("Matriz primo:"); // Muestra la matrizPrimo
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 System.out.print(matrizPrimo[i][j] + " ");
